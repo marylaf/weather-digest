fa6eda75e71ff385181c5ed8ccc66591fe1cf3d4
Maria Lafutkina <lafutkina.masha@yandex.ru>
2026-09-13 22:23:11 +0300

    fix: default HTTP request timeout to 5000 ms

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,7 +4,7 @@ export const MAX_DAYS = 7;
 
 export const DEFAULT_GEOCODING_URL = 'https://geocoding-api.open-meteo.com/v1/search';
 export const DEFAULT_FORECAST_URL = 'https://api.open-meteo.com/v1/forecast';
-export const DEFAULT_TIMEOUT_MS = 10_000;
+export const DEFAULT_TIMEOUT_MS = 5_000;
 
 export const REPORTS_DIR = './reports';
 export const UNITS = 'metric' as const;
